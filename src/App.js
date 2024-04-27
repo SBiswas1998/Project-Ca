0dc5dae7f044e8423e03c5579f79e76337816412
Banchaita <bisassanchaita1998@gmail.com>
2024-04-27 17:59:12 +0530

    few changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React,{useState, useEffect,lazy,Suspense} from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store'
-import Loading from './components/Loader';
-import { Analytics } from '@vercel/analytics/react'; // Import Analytics component
-import './App.css';
+import React, { useState, useEffect, lazy, Suspense } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import Loading from "./components/Loader";
+import { Analytics } from "@vercel/analytics/react"; // Import Analytics component
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
+import "./App.css";
 
-const AppRouter = lazy(()=>import('./routes'))
-
-
+const AppRouter = lazy(() => import("./routes"));
 
 function App() {
-
   const [isLoading, setIsLoading] = useState(true);
-  
-   useEffect(() => {
+
+  useEffect(() => {
     // Set isLoading to false after 5 seconds
     const timeout = setTimeout(() => {
       setIsLoading(false);
@@ -27,25 +25,15 @@ function App() {
 
   return (
     <Provider store={store}>
-    <BrowserRouter>
-       {/* Wrap your entire application with Analytics */}
-        {/* <Suspense fallback={
-        <div className='text-center mt-3'>
-          <Loading/>
-
-          </div>}>
-          <AppRouter/>
-        </Suspense> */}
+      <BrowserRouter>
         <Suspense fallback={<Loading />}>
-        {isLoading ? <Loading /> : <AppRouter />}
-      </Suspense>
-      {/* <AppRouter /> */}
-      <Analytics/>
-    </BrowserRouter>
-  </Provider>
+          {isLoading ? <Loading /> : <AppRouter />}
+          <Analytics />
+          <SpeedInsights />
+        </Suspense>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
 export default App;
-
-// mongodb+srv://bisassanchaita1998:ktZVUBntBcs2GMbX@cluster0.1uca38g.mongodb.net/
