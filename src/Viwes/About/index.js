31cb17ca3f166bc0be61ca12611178cf2b539af2
Banchaita <bisassanchaita1998@gmail.com>
2024-05-07 12:54:10 +0530

    other loader remove

--- src/Viwes/About/index.js
+++ src/Viwes/About/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Aboutsection, Footer } from "../../components/Layout";
 import {
   ProfileSpan,
@@ -10,41 +10,24 @@ import OurValue from "../../components/OurValue";
 import WhatWeDo from "../../components/WhatWeDo";
 import WhyUs from "../../components/WhyUs";
 import ResHeader from "../../components/ResponsiveMemu/Header";
-import TitleSection from "../../components/TitleSection";
-import OtherPageLoader from "../../components/Loader/OtherPageLoader";
 
 const About = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000); // 3 seconds
-  }, []);
 
   return (
     <>
-      
-      {isLoading ? (
-        <OtherPageLoader /> // Display loader here
-      ) : (
-        <>
-          <ResHeader />
-          {/* <TitleSection text="About" /> */}
-          <Aboutsection
-            text={ProfileText}
-            title={ProfileTitle}
-            span={ProfileSpan}
-            src={aboutImage}
-          />
-          <OurValue />
-          <WhatWeDo />
-          <WhyUs />
-          <Footer />
-        </>
-      )}
+      <ResHeader />
+      <Aboutsection
+        text={ProfileText}
+        title={ProfileTitle}
+        span={ProfileSpan}
+        src={aboutImage}
+      />
+      <OurValue />
+      <WhatWeDo />
+      <WhyUs />
+      <Footer />
     </>
-  );
+  )
 };
 
 export default About;
